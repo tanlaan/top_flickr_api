feaef2e7271346dffaf653eb4d063f4c4fc529e3
Chris Bolas <tanlaan33@gmail.com>
2022-04-11 12:22:35 -0700

    Fix rubocop guard clause

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 require 'flickr'
 
 class ApplicationController < ActionController::Base
-    def index
-        if params[:user_id] 
-            flickr = Flickr.new 
-            args = {}
-            args[:per_page] = 9
-            args[:user_id] = params[:user_id]
-            @images = flickr.photos.search args
-        end
-    end
+  def index
+    return unless params[:user_id]
+
+    flickr = Flickr.new
+    args = {}
+    args[:per_page] = 9
+    args[:user_id] = params[:user_id]
+    @images = flickr.photos.search args
+  end
 end
